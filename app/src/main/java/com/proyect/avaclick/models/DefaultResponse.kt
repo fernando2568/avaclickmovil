9e43c150159be81d3f5c7a3b1bc971fe81fc09b5
Jonathan <jonathan.requenes@softwarecalli.com>
2019-10-18 14:33:56 -0500

    Cambios en vista de Registro, peticion a server

--- app/src/main/java/com/proyect/avaclick/models/DefaultResponse.kt
+++ app/src/main/java/com/proyect/avaclick/models/DefaultResponse.kt
@@ -2,4 +2,4 @@ package com.proyect.avaclick.models
 
 import com.google.gson.annotations.SerializedName
 
-data class DefaultResponse(val success: Boolean, val exist:Boolean)
+data class DefaultResponse(val success: Boolean, val valido:Boolean, val existe: Boolean)

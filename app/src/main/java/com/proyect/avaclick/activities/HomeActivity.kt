db7a590594c56c5d2188d1c4fc87f7f0338d4493
Fernando Terrazas <gueromillan@hotmail.com>
2019-11-11 09:51:45 -0600

    Cambios Fer

--- app/src/main/java/com/proyect/avaclick/activities/HomeActivity.kt
+++ app/src/main/java/com/proyect/avaclick/activities/HomeActivity.kt
@@ -1,19 +1,51 @@
 package com.proyect.avaclick.activities
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.proyect.avaclick.R
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 import com.proyect.avaclick.storage.SharedPrefManager
 import kotlinx.android.synthetic.main.activity_home.*
 
 
 class HomeActivity : AppCompatActivity() {
 
+    //Interfaz: , NavigationView.OnNavigationItemSelectedListener
+    /*private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_sessionOff ->clseSesion()
+            R.id.nav_perfil -> Toast.makeText(this, "Parte de Perfil", Toast.LENGTH_SHORT).show()
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }*/
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+
+        /*val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)*/
 
 
         cerrarSesion.setOnClickListener {
@@ -24,8 +56,111 @@ class HomeActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
+
+
+        /*val toolbar: Toolbar = findViewById(R.id.toolbar_menu)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)*/
     }
 
+    /*private fun init() {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(this.navigationView!!, navController)
+        navigationView!!.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.activity_main_drawer, menu)
+        return true
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+
+        when (menuItem.itemId) {
+
+            R.id.nav_perfil -> {
+
+                val navOptions = NavOptions.Builder()
+                    .setPopUpTo(R.id.navigation_main, true)
+                    .build()
+
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(
+                    R.id.perfil_screen,
+                    null,
+                    navOptions
+                )
+            }
+
+            R.id.nav_sessionOff -> {
+
+                if (isValidDestination(R.id.session_screen)) {
+                    Navigation.findNavController(this, R.id.nav_host_fragment)
+                        .navigate(R.id.session_screen)
+                }
+            }
+        }
+
+        menuItem.isChecked = true
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun isValidDestination(destination: Int): Boolean {
+        return destination != Navigation.findNavController(
+            this,
+            R.id.nav_host_fragment
+        ).currentDestination!!.id
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.nav_host_fragment),
+            drawerLayout
+        )
+    }*/
+
+
+    fun clseSesion(){
+        SharedPrefManager.getInstance(applicationContext).clear()
+
+        val intent:Intent = Intent(this, LoginActivity::class.java)
+        intent.addCategory(Intent.CATEGORY_HOME)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+    }
+
+    /*override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }*/
 
     override fun onStart() {
         super.onStart()

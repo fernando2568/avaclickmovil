8c9ed8a24391cad5ac2eee39910efcb32cb9134f
Fernando Terrazas <gueromillan@hotmail.com>
2020-01-06 09:19:52 -0600

    Inicio mapa

--- app/src/main/java/com/proyect/avaclick/activities/fragments/DomicilioFragment.kt
+++ app/src/main/java/com/proyect/avaclick/activities/fragments/DomicilioFragment.kt
@@ -1,5 +1,6 @@
 package com.proyect.avaclick.activities.fragments
 
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,10 +8,15 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainer
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.proyect.avaclick.R
 
 
- class DomicilioFragment : Fragment(){
+ class DomicilioFragment : Fragment(), OnMapReadyCallback{
+
+     private lateinit var lastLocation: Location
+
      override fun onCreateView(
          inflater: LayoutInflater,
          container: ViewGroup?,
@@ -18,4 +24,11 @@ import com.proyect.avaclick.R
      ): View? {
         return inflater.inflate(R.layout.fragment_domicilio, container, false)
     }
-}
+
+     override fun onMapReady(p0: GoogleMap?) {
+         
+         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+     }
+
+
+ }
